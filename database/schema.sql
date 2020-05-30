677df7c4e00acd42c7798ac901a9c65c2f4c9d1e
prince naija <princeachuru@gmail.com>
2020-05-30 19:38:07 -0400

    schema database created

---
+++ database/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS tracker;
+CREATE DATABASE tracker;
+USE tracker;
+CREATE TABLE department
+(
+    id INT
+    unsigned auto_increment PRIMARY KEY,
+    name VARCHAR
+    (30) unique NOT NULL 
+);
+
+    CREATE TABLE role
+    (
+        id INT
+        unsigned auto_increment PRIMARY KEY,
+    title VARCHAR
+        (30) NOT NULL,
+    salary DECIMAL unsigned NOT NULL
+    department_id INT unsigned NOT NULL
+
+);
+        CREATE TABLE employee
+        (
+            id INT
+            unsigned auto_increment PRIMARY KEY,
+    first_name VARCHAR
+            (30) NOT NULL,
+    last_name VARCHAR
+            (30) NOT NULL,
+    role_id INT unsigned NOT NULL,
+    manager_id INT unsigned
+);
